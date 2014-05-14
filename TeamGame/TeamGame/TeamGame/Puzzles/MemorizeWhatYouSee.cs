e5e233aefaec5ffcb504cff8ed580059ed3a954f
dalozano <dave.lozano@outlook.com>
2014-05-14 01:47:52 -0500

    Update logic on memorize puzzle

--- TeamGame/TeamGame/TeamGame/Puzzles/MemorizeWhatYouSee.cs
+++ TeamGame/TeamGame/TeamGame/Puzzles/MemorizeWhatYouSee.cs
@@ -13,16 +13,18 @@ namespace TeamGame.Puzzles
 {
     class MemorizeWhatYouSee : IPuzzle
     {
-        Texture2D cross, hexagon, square, trapeziod;
+        Texture2D cross, hexagon, square, trapeziod, ball;
         MouseState mouse;
         List<Color> colorList;
         List<Texture2D> shapeList;
         List<Rectangle> positionList;
         Stopwatch stopWatch;
         Shape[] options = new Shape[4];
-        Shape selected;
+        Color selected;
         Shape correct;
-        Random random = new Random();
+        SoundEffectInstance memorizeIntro, memorizePrompt;
+        Boolean prompt;
+        //Random random = new Random();
 
         public MemorizeWhatYouSee(Game game, Player player)
             : base(game, player)
@@ -41,15 +43,15 @@ namespace TeamGame.Puzzles
             shapeList.Add(trapeziod);
 
             positionList = new List<Rectangle>();
-            positionList.Add(new Rectangle(drawRegion.Center.X - 75, drawRegion.Center.Y - 75, 50, 50));
-            positionList.Add(new Rectangle(drawRegion.Center.X + 25, drawRegion.Center.Y - 75, 50, 50));
-            positionList.Add(new Rectangle(drawRegion.Center.X - 75, drawRegion.Center.Y + 25, 50, 50));
-            positionList.Add(new Rectangle(drawRegion.Center.X + 25, drawRegion.Center.Y + 25, 50, 50));
+            positionList.Add(new Rectangle(drawRegion.Center.X - 63, drawRegion.Center.Y - 63, 50, 50));
+            positionList.Add(new Rectangle(drawRegion.Center.X + 13, drawRegion.Center.Y - 63, 50, 50));
+            positionList.Add(new Rectangle(drawRegion.Center.X - 63, drawRegion.Center.Y + 13, 50, 50));
+            positionList.Add(new Rectangle(drawRegion.Center.X + 13, drawRegion.Center.Y + 13, 50, 50));
 
             //randomize attributes for each shape object
             for (int i = 0; i < 4; i++)
             {
-                int[] index = { random.Next(colorList.Count), random.Next(shapeList.Count), random.Next(positionList.Count) };
+                int[] index = { Game1.random.Next(colorList.Count), Game1.random.Next(shapeList.Count), Game1.random.Next(positionList.Count) };
 
                 options[i] = new Shape(colorList[index[0]], shapeList[index[1]], positionList[index[2]]);
                 
@@ -59,8 +61,11 @@ namespace TeamGame.Puzzles
                 positionList.RemoveAt(index[2]);
             }
 
+            //initialize boolean to check if prompt audio clip needs to play
+            prompt = true;
+
             //set answer
-            correct = options[random.Next(4)];
+            correct = options[Game1.random.Next(4)];
 
             //start stopwatch
             stopWatch = new Stopwatch();
@@ -73,46 +78,114 @@ namespace TeamGame.Puzzles
             hexagon = Game.Content.Load<Texture2D>("art/hexagon");
             square = Game.Content.Load<Texture2D>("art/square");
             trapeziod = Game.Content.Load<Texture2D>("art/trapeziod");
+            ball = Game.Content.Load<Texture2D>("art/ball50px");
 
-            base.Initialize();
+            if (player == Game1.localPlayer)
+            {
+                memorizeIntro = Game.Content.Load<SoundEffect>("audio/MemorizeWhatYouSee").CreateInstance();
+                memorizePrompt = Game.Content.Load<SoundEffect>("audio/WhichColorWasTheShape").CreateInstance();
+                memorizeIntro.Play();
+            }
         }
 
         public override void Update(GameTime gameTime)
         {
-            if (stopWatch.ElapsedMilliseconds > 5000)
+            if (player != Game1.localPlayer)
+                return; // this puzzle only updates by its owner
+
+            //after 5 seconds, player can select color
+            if (stopWatch.ElapsedMilliseconds > 7000)
             {
+                if (prompt)
+                {
+                    memorizePrompt.Play();
+                    prompt = false;
+                }
+
                 mouse = Mouse.GetState();
                 if (mouse.LeftButton == ButtonState.Pressed)
-                { }             
-            }
+                {
+                    if (new Rectangle(drawRegion.Center.X - 100, drawRegion.Center.Y + 13, 50, 50).Contains(mouse.Position()))
+                        selected = Color.Red;
+                    if (new Rectangle(drawRegion.Center.X - 50, drawRegion.Center.Y + 13, 50, 50).Contains(mouse.Position()))
+                        selected = Color.Green;
+                    if (new Rectangle(drawRegion.Center.X, drawRegion.Center.Y + 13, 50, 50).Contains(mouse.Position()))
+                        selected = Color.Blue;
+                    if (new Rectangle(drawRegion.Center.X + 50, drawRegion.Center.Y + 13, 50, 50).Contains(mouse.Position()))
+                        selected = Color.Yellow;
 
-            base.Update(gameTime);
+                    if (selected == correct.color)
+                    {
+                        PuzzleOver(true);
+                    }
+                    else
+                    {
+                        PuzzleOver(false);
+                    }
+                }
+            }
         }
 
         public override void Draw(GameTime gameTime)
         {
             SpriteBatch spriteBatch = new SpriteBatch(Game.GraphicsDevice);
             
+            //show shapes
             spriteBatch.Begin();
-            if (stopWatch.ElapsedMilliseconds < 5000)
+            if (stopWatch.ElapsedMilliseconds < 7000)
             {
                 for (int i = 0; i < 4; i++)
                 {
                     spriteBatch.Draw(options[i].texture, options[i].position, options[i].color);
                 }
             }
-            if (stopWatch.ElapsedMilliseconds > 5000)
+            //after 5 seconds, show question
+            if (stopWatch.ElapsedMilliseconds > 7000)
             {
-                //ask for shape or color
+                //show shape
+                spriteBatch.Draw(correct.texture, new Rectangle(drawRegion.Center.X - 25, drawRegion.Center.Y - 63, 50, 50), Color.White);
+                //show color choices
+                spriteBatch.Draw(ball, new Rectangle(drawRegion.Center.X - 100, drawRegion.Center.Y + 13, 50, 50), Color.Red);
+                spriteBatch.Draw(ball, new Rectangle(drawRegion.Center.X - 50, drawRegion.Center.Y + 13, 50, 50), Color.Green);
+                spriteBatch.Draw(ball, new Rectangle(drawRegion.Center.X, drawRegion.Center.Y + 13, 50, 50), Color.Blue);
+                spriteBatch.Draw(ball, new Rectangle(drawRegion.Center.X + 50, drawRegion.Center.Y + 13, 50, 50), Color.Yellow);
             }
 
             spriteBatch.End();
 
             base.Draw(gameTime);
         }
+
+        public override void Encode(Lidgren.Network.NetOutgoingMessage msg)
+        {
+            //msg.Write((byte)timesDisplayed);
+            //msg.Write((byte)ballColor);
+            //msg.Write((byte)textColor);
+            //msg.Write((byte)textWord);
+        }
+
+        public override void Decode(Lidgren.Network.NetIncomingMessage msg)
+        {
+            //timesDisplayed = msg.ReadByte();
+            //ballColor = (MyColor)msg.ReadByte();
+            //textColor = (MyColor)msg.ReadByte();
+            //textWord = (MyColor)msg.ReadByte();
+        }
+
+        public override void PuzzleOver(bool p)
+        {
+            if (memorizePrompt.State == SoundState.Playing)
+                memorizePrompt.Dispose();
+            if (p)
+                Game.Content.Load<SoundEffect>("audio/Correct").Play(1.0f, 0.0f, 0.0f);
+            else
+                Game.Content.Load<SoundEffect>("audio/Incorrect").Play(1.0f, 0.0f, 0.0f);
+
+            Game.Components.Remove(this);
+
+            Game1.pStates[this.player].puzzle = new Puzzles.Transition(Game, player, p);
+        }
     }
-
-
 
     public class Shape
     {
